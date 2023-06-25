656acb54375e88b3fb3bdf8c587d29c0adea3404
Rafael Klein <rafael.augustoklein04@gmail.com>
2023-06-25 12:15:40 -0300

    Rodando o algoritmo acrescentando variaveis e while para abrir o app

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
 import pygame
-
+#import winsound
+from tkinter import simpledialog
 
 
 pygame.init()
 
-tamanho = (1000,563)
+
+pygame.init()
+tamanho = (1000, 563)
+branco = (255, 255, 255)
+raiocirculo = 10
+clock = pygame.time.Clock()
 tela = pygame.display.set_mode(tamanho)
-imagem_fundo = pygame.image.load("bg.jpg")
-icon = pygame.image.load("space.ico")
-pygame.display.set_caption("SPACE MAKER")
+pygame.display.set_caption("Space Marker")
+estrelas = {}
+fundo = pygame.image.load("bg.jpg")
+som = pygame.mixer.Sound("Space_Machine_Power.mp3")
+icon = pygame.image.load("space.png")
 pygame.display.set_icon(icon)
-pygame.mixer.music.load("Space_Machine_Power.mp3")
-pygame.mixer.music.play(-1)
-marcador_estrela = []
+som.play(-1)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            posicaomouse = pygame.mouse.get_pos()
+            item = simpledialog.askstring("Space", "Nome da Estrela:")
+            print(item)
+            estrelas[item] = posicaomouse
+            if item == "":
+                item = "Desconhecido"+str(posicaomouse)
+            estrelas[item] = posicaomouse
+    
+    for item,posicao in estrelas.items():
+        pygame.draw.circle(tela,branco,posicao,raiocirculo)
+        pygame.draw.line(tela,branco,list(estrelas.values())[0], posicao)
+        fonte = pygame.font.Font(None,20)
+        texto = fonte.render(item, True,(255,255,255))
+        tela.blit(texto,(posicao[0]+10,posicao[1]+10))
+
+    pygame.display.flip()
+    tela.fill(branco)
+    tela.blit(fundo,(0,0))
+    pygame.display.update()
+    clock.tick(60)    
+            
 
 
-    
-    
+
+
+
+
+
+
+        
+        
